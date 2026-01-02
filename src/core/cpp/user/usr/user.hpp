183366406d45c47e61c5b87eec33b4c6ba6f7724
queolvi <korpachyovdmitriy@gmail.com>
2026-01-02 15:52:02 +0300

    v 0.0.1a

--- src/core/cpp/user/usr/user.hpp
+++ src/core/cpp/user/usr/user.hpp
@@ -11,14 +11,13 @@ class user{
     public:
     user();
     ~user();
-    short unsigned get_age();
-    std::string get_gender();
-    std::string get_first_name();
-    std::string get_last_name();
-    std::string get_phone_number();
-    std::string get_email();
-    bool ask_for_TFA(bool choice, );
-    bool get_adm_answer();
+    short unsigned get_age(bool adm_answ);
+    std::string get_gender(bool adm_answ);
+    std::string get_first_name(bool adm_answ);
+    std::string get_last_name(bool adm_answ);
+    std::string get_phone_number(bool adm_answ);
+    std::string get_email(bool adm_answ);
+    bool ask_for_TFA(bool choice);
     long long get_id();
     void TFA();
 
