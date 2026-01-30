b0576284fa8975eeb1761032f99fa9ff5c0ab023
queolvi <korpachyovdmitriy@gmail.com>
2026-01-30 15:07:44 +0300

    add

--- src/core/cpp/auth/token/token.hpp
+++ src/core/cpp/auth/token/token.hpp
@@ -1,15 +1,34 @@
 #ifndef token_hpp
 #define token_hpp
-#include "C:\Programming\talkie\src\utils\utils.hpp"
-class token{
+#include "C:\Users\korpa\Desktop\programming\talkie\src\utils\utils.hpp"
+class token : public Parser{
+    Parser p{"words.txt"};
     private:
 
-    str calculate_token(vec<str> data, u_sh id) {
-        
+str calculate_token(vec<str> data, u_sh id) {
+        vec<str> result_push_data = push_data(data);
+        vec<str> data_numbers(0);
+        data_numbers.reserve(7);
+
+
     }
-    str calculate_random_symbols(str st, ll number);
-
+vec<str> calculate_random_symbols(size_t min, size_t max) {
+    vec<str> result;
+    int random_nums[7];
+    random_number r;
+    for (size_t i = 0, sz = 7; i < sz; ++i) {
+        random_nums[i] = r.roll_dice(min, max);
+    }
     
+}
 
+    vec<str> data;
+vec<str> push_data(vec<str> data) {
+    p.parse();
+    for (const auto& word : words) {
+        data.push_back(word);
+    }
+return data;
+}
 };
 #endif
--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -24,21 +24,44 @@ int roll_dice(int min, int max) {
 	std::uniform_int_distribution<int> uid(min, max);
 	return uid(rng);
 }
-
 };
 
-class Parser{
-    char symbol = ';'; str s; vec<str> st;
-    std::ifstream fs; str::size_type pos;
+class Parser {
+    public:
+    char delimiter = ';';
     str path;
-    Parser(str path_) : path(path_) {};
-    Parser() = default;
-    ~Parser() = default;
+    vec<str> words;
+
+public:
+    explicit Parser(const str& path_) : path(path_) {}
+
+    Parser(const Parser&) = delete;
+    Parser& operator=(const Parser&) = delete;
+
     void parse() {
-        fs.open(path);
-        if (!fs) { std::cout << "\nCannot open file\n"; return; }
-        while (std::getline(fs, s)) {
-            if ()
+        std::ifstream fs(path);
+        if (!fs.is_open()) {
+            std::cerr << "Cannot open file: " << path << std::endl;
+            return;
+        }
+
+        str line;
+        while (std::getline(fs, line)) {
+            if (line.empty()) continue;
+
+            str::size_type start = 0;
+            str::size_type end = 0;
+
+            while ((end = line.find(delimiter, start)) != str::npos) {
+                words.push_back(line.substr(start, end - start));
+                start = end + 1;
+            }
+            words.push_back(line.substr(start));
+        }
+    }
+    void printWords() const {
+        for (const auto& word : words) {
+            std::cout << "'" << word << "'\n";
         }
     }
 };
